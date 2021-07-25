300734775b44b087ef44dd75733a2923a3c81553
Ângelo Pietro <angelopietror@gmail.com>
2021-07-25 12:12:40 -0300

    feat: adicionada rota para lista e cadastro de produtos

--- gestao-de-cadastros/src/routes/index.js
+++ gestao-de-cadastros/src/routes/index.js
@@ -4,7 +4,8 @@ import Route from './routes';
 
 import ClientesList from '../pages/Clients/List';
 import ClientesCreate from '../pages/Clients/Register';
-
+import ProductsList from '../pages/Products/List';
+import ProductsCreate from '../pages/Products/Register';
 const Routes = () => {
   return (
           <BrowserRouter>
@@ -12,6 +13,8 @@ const Routes = () => {
               <Route path="/" exact component={ClientesList} />
               <Route path="/clients" exact component={ClientesList} />
               <Route path="/clients/new"   component={ClientesCreate}  />
+              <Route path="/products" exact component={ProductsList} />
+              <Route path="/products/new"   component={ProductsCreate}  />
             </Switch>
           </BrowserRouter>
   );
