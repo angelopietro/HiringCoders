b965c56dc59e6ca3480661f1871f6e9aee0d5df7
Ângelo Pietro <angelopietror@gmail.com>
2021-07-25 11:25:42 -0300

    feat: Validação de campos para o endereço

--- gestao-de-cadastros/src/schemas/Clients.js
+++ gestao-de-cadastros/src/schemas/Clients.js
@@ -6,12 +6,15 @@ export const validationSchema = Yup.object().shape({
   lastName: Yup.string().required('Por favor, preencha o sobrenome'),
   person: Yup.string().ensure().required('Por favor, selecione tipo de pessoa'),
   gender: Yup.string().ensure().required('Por favor, selecione o Sexo'),
-  state: Yup.string().ensure().required('Por favor, selecione um estado'),
-  city: Yup.string().ensure().required('Por favor, selecione uma cidade'),
   email: Yup.string()
     .email('Por favor, preencha um e-mail válido')
     .required('Por favor, preencha o e-mail'),
   cpf: Yup.string().required('Por favor, preencha o CPF'),
   phone: Yup.string().required('Por favor, preencha o Telefone'),
+  zipcode: Yup.string().required('Por favor, preencha o CEP'),
+  address: Yup.string().required('Por favor, preencha o Endereço'),
+  number: Yup.string().required('Por favor, preencha o Número'),
+  district: Yup.string().required('Por favor, preencha o Bairro'),
+  state: Yup.string().ensure().required('Por favor, selecione um estado'),
+  city: Yup.string().ensure().required('Por favor, selecione uma cidade'),
 });
-  
