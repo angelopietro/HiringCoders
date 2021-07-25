1822c043179b08eea6b89bb6091942291be64a05
Ângelo Pietro <angelopietror@gmail.com>
2021-07-25 11:25:10 -0300

    feat: Adicionado campos para as informações de endereço

--- gestao-de-cadastros/src/pages/Clients/Register.js
+++ gestao-de-cadastros/src/pages/Clients/Register.js
@@ -73,11 +73,11 @@ export default function RegisterClient() {
     resolver: yupResolver(validationSchema)
   });
 
-  const submitForm = (data: ClientsProps) => {
+  const submitForm = (data) => {
 
     try {
       let newData = JSON.stringify([...store || [], data])
-      localStorage.setItem('hiringCodersClientes', newData)
+      localStorage.setItem('@HC-Clients', newData)
       setStore(JSON.parse(newData))
       toast.success('Cliente cadastrado com sucesso!', {
         duration: 4000,
@@ -97,7 +97,7 @@ export default function RegisterClient() {
       <Container>
 
         <SectionHeader>
-        <h2>Gerenciando Clientes</h2>
+        <h1>Gerenciando Clientes</h1>
 
           <HeaderActions>
             <ButtomCustom onClick={redirectListClient}>
@@ -110,14 +110,14 @@ export default function RegisterClient() {
           <Row>
             <Col xs={12} md lg={6}>
               <FloatingLabel controlId="floatingInput" label="Nome *" className="mb-3">
-                <Form.Control type="text" placeholder=" " {...register('firstName')} className="{`${errors.firstName ? 'is-invalid' : ''}`}" />
-                <Form.Text className="isInvalidText">{errors.firstName?.message}</Form.Text>
+                <Form.Control type="text" placeholder=" " {...register('firstName')} className={`${errors.firstName ? 'is-invalid' : ''}`} />
+                <Form.Text className="text-danger">{errors.firstName?.message}</Form.Text>
               </FloatingLabel>
             </Col>
             <Col xs={12} md lg={6}>
               <FloatingLabel controlId="floatingInput" label="Sobrenome *" className="mb-3">
-                <Form.Control type="text" placeholder=" " {...register('lastName')} className="{`${errors.lastName ? 'is-invalid' : ''}`}" />
-                <Form.Text className="isInvalidText">{errors.lastName?.message}</Form.Text>
+                <Form.Control type="text" placeholder=" " {...register('lastName')} className={`${errors.lastName ? 'is-invalid' : ''}`} />
+                <Form.Text className="text-danger">{errors.lastName?.message}</Form.Text>
               </FloatingLabel>
             </Col>
            </Row>
@@ -129,7 +129,7 @@ export default function RegisterClient() {
                     <option value="fisica">Física</option>
                     <option value="jurídica">Jurídica</option>
                   </Form.Select>
-                  <Form.Text className="isInvalidText">{errors.person?.message}</Form.Text>
+                  <Form.Text className="text-danger">{errors.person?.message}</Form.Text>
                 </FloatingLabel>
               </Col>
               <Col xs={12} md lg={6} className="mb-3">
@@ -138,40 +138,15 @@ export default function RegisterClient() {
                     <option value="feminino">Feminino</option>
                     <option value="masculino">Masculino</option>
                   </Form.Select>
-                  <Form.Text className="isInvalidText">{errors.gender?.message}</Form.Text>
+                  <Form.Text className="text-danger">{errors.gender?.message}</Form.Text>
                 </FloatingLabel>
               </Col>
             </Row>
-
-            <Row>
-              <Col xs={12} md lg={6} className="mb-3">
-                <FloatingLabel controlId="floatingSelect" label="Estado *">
-                  <Form.Select size="lg" aria-label="Estado *" {...register('state')} onChange={(e: any) => setState(e.target.value)} className={`${errors.state ? 'is-invalid' : ''}`}>
-                    <option value="">Selecione um estado</option>
-                    {stateList.map((uf, index) =>(
-                    <option value={uf.sigla} key={index}>{uf.nome}</option>
-                    ))}
-                  </Form.Select>
-                  <Form.Text className="isInvalidText">{errors.state?.message}</Form.Text>
-                </FloatingLabel>
-              </Col>
-              <Col className="mb-3">
-                <FloatingLabel controlId="floatingSelect" label="Cidade *">
-                    <Form.Select size="lg" aria-label="Cidade *" {...register('city')} onChange={(e: any) => setCity(e.target.value)} className={`${errors.city ? 'is-invalid' : ''}`}>
-                      {cityList.map((cidade, index) => (
-                      <option value={cidade.nome} key={index}>{cidade.nome}</option>
-                      ))}
-                    </Form.Select>
-                    <Form.Text className="isInvalidText">{errors.city?.message}</Form.Text>
-                </FloatingLabel>
-              </Col>
-            </Row>
-
             <Row>
             <Col>
               <FloatingLabel controlId="floatingInput" label="Seu e-mail *" className="mb-3">
-                <Form.Control type="email" placeholder=" " {...register('email')} className="{`${errors.email ? 'is-invalid' : ''}`}" />
-                <Form.Text className="isInvalidText">{errors.email?.message}</Form.Text>
+                <Form.Control type="email" placeholder=" " {...register('email')} className={`${errors.email ? 'is-invalid' : ''}`} />
+                <Form.Text className="text-danger">{errors.email?.message}</Form.Text>
               </FloatingLabel>
             </Col>
             </Row>
@@ -179,8 +154,8 @@ export default function RegisterClient() {
             <Row>
               <Col xs={12} md lg={4}>
                 <FloatingLabel controlId="floatingInput" label="CPF *" className="mb-3">
-                  <Form.Control type="text" placeholder=" " {...register('cpf')} className="{`${errors.cpf ? 'is-invalid' : ''}`}" />
-                  <Form.Text className="isInvalidText">{errors.cpf?.message}</Form.Text>
+                  <Form.Control type="text" placeholder=" " {...register('cpf')} className={`${errors.cpf ? 'is-invalid' : ''}`} />
+                  <Form.Text className="text-danger">{errors.cpf?.message}</Form.Text>
                 </FloatingLabel>
               </Col>
               <Col xs={12} md lg={4}>
@@ -191,18 +166,74 @@ export default function RegisterClient() {
               <Col xs={12} md lg={4}>
                 <FloatingLabel controlId="floatingInput" label="Celular *" className="mb-3">
                   <Form.Control type="text" placeholder=" " {...register('phone')} className={`${errors.phone ? 'is-invalid' : ''}`} />
-                  <Form.Text className="isInvalidText">{errors.phone?.message}</Form.Text>
+                  <Form.Text className="text-danger">{errors.phone?.message}</Form.Text>
                 </FloatingLabel>
               </Col>
             </Row>
 
-            <Row>
-              <ButtonSave>CADASTRAR</ButtonSave>
+            <hr />
+            <Row className="mb-3">
+            <h2>Endereço</h2>
             </Row>
-            
+
+            <Row>
+            <Col xs={12} md lg={2}>
+              <FloatingLabel controlId="floatingInput" label="CEP *" className="mb-3">
+                <Form.Control type="text" placeholder=" " {...register('zipcode')} className={`${errors.zipcode ? 'is-invalid' : ''}`} />
+                <Form.Text className="text-danger">{errors.zipcode?.message}</Form.Text>
+              </FloatingLabel>
+            </Col>
+            <Col xs={12} md lg={8}>
+              <FloatingLabel controlId="floatingInput" label="Endereço *" className="mb-3">
+                <Form.Control type="text" placeholder=" " {...register('address')} className={`${errors.address ? 'is-invalid' : ''}`} />
+                <Form.Text className="text-danger">{errors.address?.message}</Form.Text>
+              </FloatingLabel>
+            </Col>
+            <Col xs={12} md lg={2}>
+              <FloatingLabel controlId="floatingInput" label="Número *" className="mb-3">
+                <Form.Control type="text" placeholder=" " {...register('number')} className={`${errors.number ? 'is-invalid' : ''}`} />
+                <Form.Text className="text-danger">{errors.number?.message}</Form.Text>
+              </FloatingLabel>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md lg={4}>
+              <FloatingLabel controlId="floatingInput" label="Bairro *" className="mb-3">
+                <Form.Control type="text" placeholder=" " {...register('district')} className={`${errors.district ? 'is-invalid' : ''}`} />
+                <Form.Text className="text-danger">{errors.district?.message}</Form.Text>
+              </FloatingLabel>
+            </Col>
+              <Col xs={12} md lg={4} className="mb-3">
+                <FloatingLabel controlId="floatingSelect" label="Estado *">
+                  <Form.Select size="lg" aria-label="Estado *" {...register('state')} onChange={(e: any) => setState(e.target.value)} className={`${errors.state ? 'is-invalid' : ''}`}>
+                    <option value="">Selecione um estado</option>
+                    {stateList.map((uf, index) =>(
+                    <option value={uf.sigla} key={index}>{uf.nome}</option>
+                    ))}
+                  </Form.Select>
+                  <Form.Text className="text-danger">{errors.state?.message}</Form.Text>
+                </FloatingLabel>
+              </Col>
+              <Col className="mb-3">
+                <FloatingLabel controlId="floatingSelect" label="Cidade *">
+                    <Form.Select size="lg" aria-label="Cidade *" {...register('city')} onChange={(e: any) => setCity(e.target.value)} className={`${errors.city ? 'is-invalid' : ''}`}>
+                      {cityList.map((cidade, index) => (
+                      <option value={cidade.nome} key={index}>{cidade.nome}</option>
+                      ))}
+                    </Form.Select>
+                    <Form.Text className="text-danger">{errors.city?.message}</Form.Text>
+                </FloatingLabel>
+              </Col>
+              <Col xs={12}>
+                <FloatingLabel controlId="floatingInput" label="Complemento" className="mb-3">
+                  <Form.Control type="text" placeholder=" " {...register('complement')}  />
+                </FloatingLabel>
+              </Col>
+            </Row>
+
+            <Row><ButtonSave>CADASTRAR</ButtonSave></Row>
+
           </Form>
-
-
       </Container>
 
   );
